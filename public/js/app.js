cc1a5bbea639cdc3371a080962d6455563a162c7
Joshua Croad <joshcroad@gmail.com>
2014-04-26 15:35:10 +0100

    Implemented basic colour changing game

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,8 @@ var App = (function () {
   return {
 
     prop: {
-      message: document.getElementById('hello')
+      message: document.getElementById('hello'),
+      players: document.getElementById('players')
     },
 
     init: function () {
@@ -18,9 +19,11 @@ var App = (function () {
     initEvents: function () {
       App.prop.socket.on('connect', App.onConnect.bind(App));
       App.prop.socket.on('gameStart', App.onGameStart.bind(App));
+      App.prop.socket.on('receiveColour', App.onChangeColour.bind(App));
     },
 
     bindEvents: function () {
+
     },
 
     onConnect: function () {
@@ -35,14 +38,23 @@ var App = (function () {
     onGameStart: function (data) {
       if (navigator.userAgent.match(/iPhone/i)) {
         // App.Pad.init(); // DISPLAY GAME PAD ON MOBILE
-        // iterate over data.players and put them in string var=players;
-        // <div data-player_id="">
-        App.prop.players.innerHTML = players;
-        App.prop.message.innerHTML = '<a data-player_id=\'\' href=\'\' id=\'gogogo\'>GO GO GO</a>';
+        App.prop.message.innerHTML = '<a data-player_id=\'' + data._id + '\' href=\'\' id=\'gogogo\'>GO GO GO</a>';
+        document.getElementById('gogogo').addEventListener('touchstart', function (e) {
+          App.prop.socket.emit('gogogo', data);
+        });
       } else {
+        App.prop.players.innerHTML += '<ul>';
+        data.players.forEach(function (player) {
+          App.prop.players.innerHTML += '<li data-player_id="' + player._id + '">' + player._id + '</li>';
+        });
+        App.prop.players.innerHTML += '</ul>';
         // App.Screen.init(); // DISPLAY GAME SCREEN
         App.prop.message.innerHTML = 'game started';
       }
+    },
+
+    onChangeColour: function (data) {
+      document.querySelectorAll('#gogogo[data-player_id=' + data._id + ']').style.color = data.colour;
     }
 
   };
